974d7a9064636825d5b01cbef7a2164536a5246f
igor g <igglike@gmail.com>
2023-03-01 12:58:05 +0600

    Created book cretion form and view

--- bookkeeper/views.py
+++ bookkeeper/views.py
@@ -1,29 +1,50 @@
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from .models import Publisher, Author, Book
+from .forms import BookForm
 
 
 def book_list(request: HttpRequest):
-    books = Book.objects.all()
-    context = {'books': books}
+    objects = Book.objects.all()
+    context = {
+        'objects': objects,
+        'page_title': 'List of all books' 
+    }
     return render(request, 'books/book_list.html', context)
 
 
 def book_detail(request: HttpRequest, book_id: int):
-    book = get_object_or_404(Book, pk=book_id)
-    context = {'book': book}
+    obj = get_object_or_404(Book, pk=book_id)
+    context = {
+        'obj': obj,
+        'page_title': 'Details of a book' 
+    }
     return render(request, 'books/book_detail.html', context)
 
 
 def create_book(request: HttpRequest):
-    form = 'FORM'
-    context = {'form': form}
+    form = BookForm()
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            obj = form.save()
+            print("BOOOOOK CREATED", obj.pk)
+            return redirect('book_detail', book_id=obj.pk)
+    context = {
+        'form': form,
+        'page_title': 'Create a book'
+    }
     return render(request, 'books/create_book.html', context)
 
 
 def edit_book(request: HttpRequest, book_id: int):
-    book = get_object_or_404(Book, pk=book_id)
-    context = {'book': book}
+    obj = get_object_or_404(Book, pk=book_id)
+    form = 'FORM'
+    context = {
+        'obj': obj,
+        'form': form,
+        'page_title': 'Edit a book '
+    }
     return render(request, 'books/edit_book.html', context)
 
